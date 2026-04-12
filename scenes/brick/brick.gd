5d5b44f0512045cc4bc08499805ea762a51f23b0
hei hei OK OK! <98307235+laisammy@users.noreply.github.com>
2026-04-12 02:37:06 +0100

    Add textures and environment

---
+++ scenes/brick/brick.gd
@@ -0,0 +1,24 @@
+extends RigidBody3D
+
+class_name Brick
+
+const TABLE_GROUP: String = "table"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_sleeping_state_changed() -> void:
+	print("_on_sleeping_state_changed: ", sleeping)
+
+func _on_body_entered(body: Node) -> void:
+	print("Brick has collided with " + str(body))
+	if body.is_in_group(TABLE_GROUP):
+		print("Game over")
